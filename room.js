180cd1be4af8e079ec6dde108cb4fe210f2cd97c
Tanawat Wirattangsakul <webmaster.tanawat@gmail.com>
2019-01-31 16:25:01 +0700

    Add files via upload

---
+++ room.js
@@ -0,0 +1,7 @@
+room = (size) => {
+    switch(size){
+        case 500: console.log('S'); break
+        case 1000: console.log('L'); break
+        default: console.log('Extra'); break
+    }
+    }
